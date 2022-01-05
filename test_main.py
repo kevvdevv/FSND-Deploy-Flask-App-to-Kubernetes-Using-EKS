bdb73ea51e74b25dad417c96bead58c24ef1f9e7
Kevin Olson <kevin.s.olson@gmail.com>
2022-01-05 14:30:14 -0500

    Fixed secrets in test

--- test_main.py
+++ test_main.py
@@ -7,10 +7,7 @@ import pytest
 
 import main.__init__ as __init__
 
-SECRET = 'TestSecret'
-TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 
 @pytest.fixture
 def client():
